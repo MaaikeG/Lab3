28f1d815d59ea07094db121c39cb714626bbc4f1
MaaikeG <maaikegalama@outlook.com>
2017-10-09 15:34:34 +0200

    Ticket

---
+++ Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class Ticket
+    {
+        public void calculateTicketPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
